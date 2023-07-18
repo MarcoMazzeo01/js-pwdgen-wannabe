887f29839fbfa944dcc27b55fd496683585282b2
plastikSun <marcom2k01@gmail.com>
2023-07-18 15:02:49 +0200

    add: prompt variables

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// constants
+const pwEnd = "21";
+
+//variables
+let name = prompt("Inserisci il tuo nome","Nome utente");
+let lastName = prompt("Inserisci il tuo cognome","Cognome utente");
+let favColor = prompt("Inserisci il tuo colore preferito","Colore preferito");
+
